164d02454b322503140542ace89c6ce1852331aa
Victor Ortega <51211089+vorteg@users.noreply.github.com>
2024-02-20 19:15:08 +0000

    Format and Adding Title

--- game_functions.py
+++ game_functions.py
@@ -1,5 +1,7 @@
 import time
 import os
+from colorama import Fore, Style
+from pyfiglet import figlet_format
 
 def clean_screen():
     if os.name == "nt":
@@ -11,10 +13,14 @@ def scroll_art_ascill(art_ascii):
     lines = art_ascii.split("\n")
     for line in range(len(lines) + 1):
         clean_screen()
-        print("\n".join(lines[:line]))
+        print(Fore.CYAN + "\n".join(lines[:line]) + Style.RESET_ALL)
         time.sleep(0.2)
 
 def load_image(image_file):
     with open(image_file, "r") as file:
         img = file.read()
     return img
+
+def print_title():
+    title = figlet_format("Digitos Mortales")
+    return Fore.RED + title + Style.RESET_ALL
